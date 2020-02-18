efd1e6811cf2947563e91bc0c296fa3475116ecf
Shervin Sarain <shervin.sarain@labri.fr>
2020-02-18 17:56:53 +0100

    user interface

--- src/crawler.py
+++ src/crawler.py
@@ -5,36 +5,16 @@ from queryparser import QueryParser
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
-if len(sys.argv) == 2:
-    output_root_directory = '../output'
-
-    # generate keywords from user input
-    keywords = ['graph', 'visualisation']
-    directory = 'test'
-
-    output_directory = '{}/{}_{}'.format(output_root_directory,
-                                        directory, datetime.utcnow().strftime('%Y-%m-%d_%X'))
-
-    parser = QueryParser()
-    parser.test()
-
-    # try:
-    #     os.mkdir(output_directory)
-    # except OSError:
-    #     print('Creation of output directory %s failed' % output_directory)
-    # else:
-    #     settings = get_project_settings()
-    #     settings.set('OUTPUT_DIRECTORY', output_directory)
-    #     process = CrawlerProcess(settings)
-    #     # process.crawl('GoogleScholar', keywords_array=[keywords])
-    #     # process.crawl('ScienceDirect', keywords_array=[keywords])
-    #     # process.crawl('SpringerLink', keywords_array=[keywords])
-    #     # process.crawl('ACMLibrary', keywords_array=[keywords])
-    #     # process.crawl('WileyLibrary', keywords_array=[keywords])
-    #     # process.crawl('BASE', keywords_array=[keywords])
-    #     # process.crawl('arXiv', keywords_array=[keywords])
-    #     # process.crawl('IEEEXplore', keywords_array=[keywords])
-    #     # process.crawl('MicrosoftAcademic', keywords_array=[keywords])
-    #     process.start()
-else:
-    print('Incorrect arguments')
+def crawl(output_directory, search_engines, keyword_list):
+    try:
+        os.mkdir(output_directory)
+    except OSError:
+        print('Creation of output directory %s failed' % output_directory)
+        sys.exit()
+    else:
+        settings = get_project_settings()
+        settings.set('OUTPUT_DIRECTORY', output_directory)
+        process = CrawlerProcess(settings)
+        for search_engine in search_engines:
+            process.crawl(search_engine, keywords_array=keyword_list)
+        process.start()
--- src/main.py
+++ src/main.py
@@ -1,89 +1,118 @@
 import os
 import sys
 import re
-from PyInquirer import prompt, print_json
-from PyInquirer import Validator, ValidationError
+from datetime import datetime
+from prompt_toolkit.validation import Validator
+from prompt_toolkit.shortcuts import radiolist_dialog, input_dialog, checkboxlist_dialog, yes_no_dialog
 from datetime import datetime
 from queryparser import QueryParser
+from crawler import crawl
+
+output_root_directory = '../output'
 
 
-class DirectoryValidator(Validator):
-    def validate(self, document):
-        valid = re.match(r'^[a-zA-Z_0-9-]+$', document.text) is not None
-        if not valid:
-            raise ValidationError(
-                message='Please enter a valid directory name',
-                cursor_position=len(document.text))
+def is_valid_directory(input):
+    return re.match(r'^[a-zA-Z_0-9-]+$', input) is not None
 
 
-class QueryValidator(Validator):
-    def validate(self, document):
-        valid = QueryParser().validate(document.text)
-        if valid is not True:
-            raise ValidationError(
-                message='Query parsing error. Make sure your query is written correctly.',
-                cursor_position=len(document.text))
+def is_valid_query(input):
+    return QueryParser().validate(input)
 
 
-questions = [
-    {
-        'type': 'list',
-        'name': 'process',
-        'message': 'What do you want to do?',
-        'choices': [
-            {
-                'name': 'Automated paper search',
-                'value': 'search'
-            },
-            {
-                'name': 'Result analysis',
-                'value': 'analysis'
-            }
+def search_confirm_message(query):
+    message = 'This query will search for the following keyword combinations: \n'
+    count = 1
+    for keyword_list in query:
+        message = message + '    {}) {}\n'.format(count, ' '.join(keyword_list))
+        count = count + 1
+    message = message + 'Is this okay?'
+    return message
+
+
+directory_validator = Validator.from_callable(
+    is_valid_directory,
+    error_message="Not a valid directory name.",
+    move_cursor_to_end=True,
+)
+
+results = radiolist_dialog(
+    title='Automated paper systematic review',
+    text='What do you want to do ?',
+    values=[
+        ('search', 'Paper search'),
+        ('analysis', 'Search result analysis')
+    ]
+).run()
+
+if results == 'search':
+    query = input_dialog(
+        title='Automated paper systematic review',
+        text='Please input your desired search query:'
+    ).run()
+    while query is not None and not is_valid_query(query):
+        query = input_dialog(
+            title='Automated paper systematic review',
+            text='Please input your desired search query:\nInvalid query.'
+        ).run()
+    if query is None:
+        sys.exit()
+    query = QueryParser().parse(query)
+
+    confirm = yes_no_dialog(
+        title='Automated paper systematic review', text=search_confirm_message(query)
+    ).run()
+    if not confirm:
+        sys.exit()
+
+    search_engines = checkboxlist_dialog(
+        title='CheckboxList dialog',
+        text='Select search engines to crawl.',
+        values=[
+            ('ACMLibrary', 'ACM Digital Library'),
+            ('arXiv', 'arXiv'),
+            ('BASE', 'BASE'),
+            ('GoogleScholar', 'Google Scholar'),
+            ('IEEEXplore', 'IEEE Xplore Digital Library'),
+            ('MicrosoftAcademic', 'Microsoft Academic'),
+            ('ScienceDirect', 'Science Direct'),
+            ('SpringerLink', 'Springer Link'),
+            ('WileyLibrary', 'Wiley Online Library'),
         ]
-    },
-    {
-        'type': 'input',
-        'name': 'directory',
-        'message': 'Input the output directory name.',
-        'validate': DirectoryValidator,
-        'when': lambda answers: answers['process'] == 'search'
+    ).run()
+    while search_engines is not None and len(search_engines) == 0:
+        search_engines = checkboxlist_dialog(
+            title='CheckboxList dialog',
+            text='Select search engines to crawl.\nPlease select at least one.',
+            values=[
+                ('ACMLibrary', 'ACM Digital Library'),
+                ('arXiv', 'arXiv'),
+                ('BASE', 'BASE'),
+                ('GoogleScholar', 'Google Scholar'),
+                ('IEEEXplore', 'IEEE Xplore Digital Library'),
+                ('MicrosoftAcademic', 'Microsoft Academic'),
+                ('ScienceDirect', 'Science Direct'),
+                ('SpringerLink', 'Springer Link'),
+                ('WileyLibrary', 'Wiley Online Library'),
+            ]
+        ).run()
+    if search_engines is None:
+        sys.exit()
 
-    },
-    {
-        'type': 'input',
-        'name': 'query',
-        'message': 'Input your desired search query.',
-        'validate': QueryValidator,
-        'filter': lambda input: QueryParser().parse(input),
-        'when': lambda answers: answers['process'] == 'search'
+    directory = input_dialog(
+        title='Automated paper systematic review',
+        text='Please input the output directory name:'
+    ).run()
+    while directory is not None and not is_valid_directory(directory):
+        directory = input_dialog(
+            title='Automated paper systematic review',
+            text='Please input the output directory name:\nInvalid directory name.'
+        ).run()
+    if directory is None:
+        sys.exit()
+    diretory = '{}/{}_{}'.format(output_root_directory,
+                                 directory, datetime.utcnow().strftime('%Y-%m-%d_%X'))
 
-    },
-    {
-        'type': 'checkbox',
-        'name': 'search_engines',
-        'message': 'Select search engines to crawl.',
-        'choices' : [
+    crawl(directory, search_engines, query)
 
-        ],
-        'when': lambda answers: answers['process'] == 'search'
-
-    },
-    # {
-    #     'type': 'input',
-    #     'name': 'output_directory',
-    #     'message': 'What\'s your first name',
-    # }
-]
-
-answers = prompt(questions)
-print(answers)
-
-# qs = [
-#     # '"a g" | "b" | "c"',
-#     # '"a" & "b" | "c"',
-#     # '"a" & "b" | "c" & "d" ',
-#     # '("a" | "b") | ("c" | "d")',
-#     # '"a" & ("b" | "c") & ("d" | "e")',
-#     # '("b" & ("c" | "d") | "e" & ("f" | "g"))',
-#     # '"a" & ("b" & ("c" | "d") & "e" & ("f" | "g")) | x'
-# ]
+elif results == 'analysis':
+    print('todo')
